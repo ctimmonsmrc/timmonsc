75b133fd0b0d1548b2b636e2fd405a869ad7464a
Craig Timmons <craig.timmons@mrcglobal.com>
2018-11-28 12:01:32 -0500

    Update subnetlocation.ps1

---
+++ subnetlocation.ps1
@@ -0,0 +1,11 @@
+﻿cls
+$subnets=Import-Csv c:\scriptinput\subnet.csv
+foreach ($subnet in $subnets)
+	{
+	set-cslissubnet -Subnet $subnet.subnet -Description $subnet.description -HouseNumber $subnet.HouseNumber `
+	-StreetName $subnet.StreetName  -StreetSuffix $subnet.StreetSuffix -City $subnet.City -State $subnet.state `
+	-PostalCode $subnet.PostalCode -Country $subnet.country -Location $subnet.Location  -CompanyName $subnet.CompanyName 
+	
+	Publish-CsLisConfiguration
+#	
+	}
